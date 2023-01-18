8d7bae5cfca779dc8d30841ad668a3766ceecd33
Harry Seiler <harald.seiler@aikq.de>
2023-01-18 09:19:30 +0100

    python helper extract_prefix_table.py

--- dxcc/src/python_helpers/extract_prefix_table/extract_prefix_table.py
+++ dxcc/src/python_helpers/extract_prefix_table/extract_prefix_table.py
@@ -24,6 +24,18 @@ try:
                 callpref = element.split('_')
                 suffix = callpref[1]
 
+            lastCharMinElement = 0
+            lastCharMaxElement = 0
+            element_range = element.split('-') # wir werten den Bereich aus um die Prefixes zu expandieren (AA-AC -> AA, AB, AC)
+            if len(element_range) > 1:
+                if str.isalpha(element_range[0][-1]):
+                    lastCharMinElement = ascii_index.find(element_range[0][-1])
+                    lastCharMaxElement = ascii_index.find(element_range[1][-1])
+                else:
+                    lastCharMinElement = int(element_range[0][-1])
+                    lastCharMaxElement = int(element_range[1][-1])
+            # TEST
+            print(element_range, lastCharMinElement, lastCharMaxElement, sep='\t')
             # Prefixstring muss getestet werden auf einen Bereich der dann noch expandiert werden muss (AA-AC -> AA, AB, AC)
 except BaseException as err:
     print('Fehler:', err)
