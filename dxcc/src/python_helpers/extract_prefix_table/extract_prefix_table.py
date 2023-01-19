8acce85d79ab5c40d54aca7ef5da7bfeeb577a7d
Harry Seiler <harald.seiler@aikq.de>
2023-01-19 13:36:43 +0100

    fixed extract_prefix_table.py output error, update todo.txt

--- dxcc/src/python_helpers/extract_prefix_table/extract_prefix_table.py
+++ dxcc/src/python_helpers/extract_prefix_table/extract_prefix_table.py
@@ -13,7 +13,7 @@ try:
     index = 0
     for line in src:
         
-        linesplit = line.split(';') # eine Zeile wird aufgeteilt am Semikolon in (interne) IndexID und Liste mit den Prefixstrings 
+        linesplit = line.split(';') # eine Zeile wird aufgeteilt am Semikolon in (interne) dxccID und Liste mit den Prefixstrings 
         index = int(linesplit[1])
         raw_prefix_list = linesplit[0].split(',')
 
@@ -24,18 +24,18 @@ try:
                 suffix = callpref[1]
 
             prfx_printout = []
-            element_range = element.split('-') # wir werten den Bereich aus um die Prefixes zu expandieren (AA-AC -> AA, AB, AC)
+            element_range = element.split('-') # wir werten den Bereich aus um die Prefixes zu expandieren (AA-AC -> AA, AB, AC oder SV1-SV3 -> SV1,SV2,SV3)
             if len(element_range) > 1:
                 lastCharMinElement = 0
                 lastCharMaxElement = 0
                 temp = element_range[0][:-1]
-                if str.isalpha(element_range[0][-1]):
+                if str.isalpha(element_range[0][-1]):   # es sollen Buchstabenreihen expandiert werden
                     lastCharMinElement = ascii_index.find(element_range[0][-1])
                     lastCharMaxElement = ascii_index.find(element_range[1][-1])
                     asc_indexes = [ i for i in range(lastCharMinElement, lastCharMaxElement + 1) ]
                     for i in asc_indexes:
-                        prfx_printout += [ temp + ascii_index[i] ]  
-                else:
+                        prfx_printout += [ temp + ascii_index[i] ]    #  von AA-AC wird A abgetrennt und durch AA, AB, AC ersetzt 
+                else:       # sind Zahlen als letztes Zeichen im String werden diese expandiert
                     lastCharMinElement = int(element_range[0][-1])
                     lastCharMaxElement = int(element_range[1][-1])
                     numbers = [ i for i in range(lastCharMinElement, lastCharMaxElement + 1) ]
@@ -43,14 +43,11 @@ try:
                         prfx_printout += [ temp + str(i) ]
                 
             else:
-                prfx_printout += element_range
+                prfx_printout += element_range   # es war nichts zu expandieren, wir übernehmen einfach den String
 
-            
-# this is commented out because it causes a wrong printout of lines which includes a '_'
-# we have to find another way to erase the _Suffix from the prefixes...
-
+            #printout data - erasing the '_Suffix'
             for element in prfx_printout:
-                if element.find('_') != -1:
+                if element.find('_') != -1:    # wir müssen noch den Suffix abschneiden da dieser ja in der Variable suffix bereits enthalten ist
                     print('Prefix:', element[:-2], 'Suffix:', suffix, 'dxcc_index:', index)    
                 else:
                     print('Prefix:', element, 'Suffix:', suffix, 'dxcc_index:', index)
