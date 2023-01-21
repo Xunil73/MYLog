482622d8382b72466c608c25ee491ec53ebe9046
Harry Seiler <dj5my@ok.de>
2023-01-21 07:52:32 +0100

    Revert "rollback"
    This reverts commit cae93dbe058c92dd608531afccf4e02dee7b4e00, reversing
    changes made to dddf09215d34e244d3225f65f587db3b366d4a3a.

---
+++ dxcc/src/python_helpers/extract_cq_table/extract_cq_table.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+
+try:
+    src = open('./../../src_rawdata/cq_sorted.txt', 'rt')
+    dest = open('cq_tableready.txt', 'at')
+
+    id = 1
+    index = 0
+    for line in src:
+        linesplit = line.split(';')
+        ituZones = linesplit[0].split(',')
+        index = int(linesplit[1])
+
+        for element in ituZones:
+            zoneMinMax = element.split('-')
+            
+            result = [ int(erg) for erg in zoneMinMax ]
+            common_result = [ i for i in range(min(result), max(result) + 1) ]
+            for a in common_result:
+                # print(a, ',', index, ',', id)
+                output = str(a) + ',' + str(index) + ',' + str(id) + '\n'
+                dest.write(output)
+                id += 1
+
+
+
+except BaseException as err:
+    print('Fehler:', err)
+ 
+    
+finally: 
+    src.close()
+    dest.close()
---
+++ dxcc/src/python_helpers/extract_itu_table/README.txt
@@ -0,0 +1,12 @@
+# extract_itu_table.py is a script that creates a 
+# comma separated file that can be used to fill a 
+# sql-table with columns:
+# |ituZoneNumber|dxccID|uniqueID|
+# where dxccID is a own created id matching the dxcc entities in the 
+# list countries_sorted.txt
+# uniqueID is the increment ID to point to an unique row in the list
+
+# the script catches the source file from MYLog/dxcc/src/src_rawdata/itu_sorted.txt
+# and outputs the comma separated file in the same folder as the script is
+# (this folder)
+
