e4d69036441109d4354af5828178592422c5a9e3
Harry Seiler <dj5my@ok.de>
2023-03-18 09:38:31 +0100

    started coding printfuncs.h

--- my_src/printfuncs.h
+++ my_src/printfuncs.h
@@ -1,5 +1,31 @@
 #ifndef PRINTFUNCS_H
 #define PRINTFUNCS_H
 
+class Userinterface {
+    public:
+        Userinterface();
+        void drawHeader();
+        void drawBody();
+        void drawInputSection();
+};
 
+inline Userinterface::Userinterface() {}
+
+inline void Userinterface::drawHeader() {
+    std::cout.width(70);
+    std::cout.fill('*');
+    std::cout << "" << std::endl;
+
+    for(int j=0; j<4; ++j) {
+        std::cout << "*";
+        for(int i=0; i<68; ++i) {
+            std::cout << " ";
+        }
+        std::cout << "*" << std::endl;
+    }
+
+    std::cout.width(70);
+    std::cout.fill('*');
+    std::cout << "" << std::endl;
+}
 #endif // PRINTFUNCS_H
