7167c6b86b9bfd09ba83345d0838b83498dc6330
harry <harald.seiler@aikq.de>
2023-08-21 00:44:47 +0200

    splitCall() splits input in Pre,Call,Suff

--- my_src/hamfunc.h
+++ my_src/hamfunc.h
@@ -3,7 +3,101 @@
 
 
 #include <string>
+#include <vector>
 #include "Datenbank.h"
+
+
+/* splitCall() trennt AA/AB1CD/P auf in Suffix, Call und
+   Prefix.
+   DJ5MY/M wird zu Prefix=DJ, Basecall DJ5MY und Suffix=M
+   aufgeteilt.
+*/
+Array2d<std::string> splitCall(const std::string& call) {
+
+    enum column {PREFIX, SUFFIX, DXCC_REF, PK_ID};
+
+    const char* basetable {"./../dxcc/src/db/dxcc_basetable.db"};
+
+    Array2d<std::string> ausgabe(1,1,""); // Platzhalter
+
+    std::string prefix {""};
+    std::string suffix {""};
+    std::string basecall {call};
+    Array2d<std::string> erg(1,3,"");
+
+    try {
+        Datenbank db(basetable);
+
+        ausgabe.assign(db.execute("SELECT * FROM prefixes"));
+
+        bool specialSuffFound {false};
+        // ist ein Spezialsuffix enthalten der zur Bestimmung eines DXCC dient?
+        for(size_t row=1; row<ausgabe.getZeilen(); ++row) { // Überschrift ausblenden
+            if(ausgabe.at(row, SUFFIX) != "") {
+                std::string comp {"/" + ausgabe.at(row, SUFFIX)};
+                auto pos = call.length() - comp.length();
+                if(call.rfind(comp) == pos) {
+                    suffix = ausgabe.at(row, SUFFIX);
+                    basecall.replace(pos, comp.length(), "",0, 0); // wir schneiden den Suffix vom Call-String ab
+                    specialSuffFound = true;
+                    break;
+                }
+            }
+        }
+
+        // ist ein gängiger Suffix enhalten (/p, /m, /am, /mm) ??
+        if(!specialSuffFound) {
+            std::vector<std::string> stdsuff {"/P", "/M", "/AM", "/MM", "/QRP"};
+
+            for(size_t i=0; i<stdsuff.size(); ++i) {
+                auto pos = call.length() - stdsuff.at(i).length();
+                if(call.rfind(stdsuff.at(i)) == pos) {
+                    basecall.replace(pos, stdsuff.at(i).length(), "",0 ,0); // wir schneiden den Suffix vom Call ab
+                    suffix = stdsuff.at(i).replace(0,1,"",0,0); // das "/" aus "/P" entfernen
+                    break;
+                }
+            }
+        }
+     /* wir durchsuchen die Tabelle mit den Prefixen und vergleichen mit unserem Call.
+        Wird die Zeichenkette des Prefixes am Anfang des Calls gefunden wird die Anzahl
+        der übereinstimmenden Zeichen gemerkt. Gibt es bei weiteren Durchläufen einen
+        Prefix der noch mehr übereinstimmende Zeichen hat wird dieser als passender
+        Prefix festgelegt. Dies garantiert einen Match bei Calls mit und ohne "/" */
+
+        size_t hold_max {0};
+        for(size_t row=1; row<ausgabe.getZeilen(); ++row) {
+            if(call.find(ausgabe.at(row, PREFIX)) == 0) {     // gesuchter Präfix steht am Anfang des Calls
+                size_t i {0};
+                size_t zaehler {0};
+                while(i < call.length() && i < ausgabe.at(row, PREFIX).length()) {
+                    if(call[i] == (ausgabe.at(row, PREFIX))[i])  ++zaehler;
+                    else break;
+                    ++i;
+                }
+
+                if(hold_max < zaehler) {
+                    hold_max = zaehler;
+                    prefix = ausgabe.at(row, PREFIX);
+                }
+            }
+        }
+        // wir schneiden vorangestellten Suffix ab
+        if(call.at(hold_max) == '/') basecall.replace(0, hold_max+1, "", 0, 0);
+
+        erg.at(0,0) = prefix;
+        erg.at(0,1) = basecall;
+        erg.at(0,2) = suffix;
+
+    }
+
+    catch (const SQLError& e) {
+      std::cerr << e.what() << '\n';
+    }
+
+    return erg;
+}
+
+
 
 Array2d<std::string> getDxccRefIDs(const std::string& call) {
 
@@ -33,9 +127,9 @@ Array2d<std::string> getDxccRefIDs(const std::string& call) {
             }
         }
 
-     /* wir durchsuchen die Tabelle mit den Prefixen und vergleichen mit unserem Call. 
+     /* wir durchsuchen die Tabelle mit den Prefixen und vergleichen mit unserem Call.
         Wird die Zeichenkette des Prefixes am Anfang des Calls gefunden wird die Anzahl
-        der übereinstimmenden Zeichen gemerkt. Gibt es bei weiteren Durchläufen einen 
+        der übereinstimmenden Zeichen gemerkt. Gibt es bei weiteren Durchläufen einen
         Prefix der noch mehr übereinstimmende Zeichen hat wird dieser als passender
         Prefix festgelegt. Dies garantiert einen Match bei Calls mit und ohne "/" */
 
